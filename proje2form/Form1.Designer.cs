c9d39d88249de1ac59d0f656c344c66beb6f4361
rumeysakazdal <kazdalrumeysa@gmail.com>
2023-03-10 19:48:45 +0300

    c# kod

---
+++ proje2form/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace proje2form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKısa = new System.Windows.Forms.TextBox();
+            this.txtUzun = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(41, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kısa Kenar\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(41, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzun Kenar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(365, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cevre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(365, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Alan";
+            // 
+            // txtKısa
+            // 
+            this.txtKısa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKısa.Location = new System.Drawing.Point(177, 111);
+            this.txtKısa.Name = "txtKısa";
+            this.txtKısa.Size = new System.Drawing.Size(131, 34);
+            this.txtKısa.TabIndex = 4;
+            this.txtKısa.Text = "\r\n";
+            // 
+            // txtUzun
+            // 
+            this.txtUzun.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUzun.Location = new System.Drawing.Point(177, 166);
+            this.txtUzun.Name = "txtUzun";
+            this.txtUzun.Size = new System.Drawing.Size(131, 34);
+            this.txtUzun.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(177, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hesapla\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 324);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUzun);
+            this.Controls.Add(this.txtKısa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKısa;
+        private System.Windows.Forms.TextBox txtUzun;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ proje2form/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje2form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kisa = Convert.ToInt32(txtKısa.Text); //kisa kenarı tanımladım
+            int uzun = Convert.ToInt32(txtUzun.Text); //uzun kenarı tanımladım
+            int alan = kisa * uzun; //alan hesaplamasını girdim
+            int cevre = (kisa + uzun) * 2; //cevre hesaplamasını girdim
+            label3.Text = "cevre: " + cevre; //ekrana cevre sonucunu yazdırdım
+            label4.Text = "alan: " + alan; //ekrana alan sonucunu yazdırdım
+
+
+
+
+        }
+    }
+}
